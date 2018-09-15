4612d6c4704e2cadf2f6db579cb7f2e5fd65994e
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 06:43:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,8 @@
 chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
+function addElementToEndOfArray(array, element){
+  
+}
 function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
