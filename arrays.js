c1008876fc3f8e3aa1f2b8c7aaabf47c553194e7
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 06:21:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,9 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+function addElementToBeginningOfArray(array, element) {
+	  return [element, ...array]
+	}
 
 function addElementToEndOfArray(array, element){
  [...array, element];
