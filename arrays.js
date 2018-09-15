7927ce9721d70a9c1c0c90af0b0c06eeba4f019a
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 06:47:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 	
 	
 function removeElementFromBeginningOfArray(array) {
-	  return array.slice(0, array.length - 1)
+	 array.slice(0, array.length - 1)
+	 return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
