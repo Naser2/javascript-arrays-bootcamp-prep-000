784d9732786d1dec78e6a3c89581622b3c10e933
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 06:40:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function addElementToBeginningOfArray(array, element) {
 	}
 
 function removeElementFromBeginningOfArray(arrayt){
- array.pop([element, ...array,]);
+ array.pop(element, ...array);
  return array
 }
 
