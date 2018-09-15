eaf6767756dda939ebd2b8fca6af9d57662fb885
Nasser Sanou <nassersanou23@gmail.com>
2018-09-15 06:19:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToEndOfArray(array, element){
  return array
 }
 
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-  return array;
-}
+// function destructivelyAddElementToEndOfArray(array, element){
+//   array.push(element);
+//   return array;
+// }
